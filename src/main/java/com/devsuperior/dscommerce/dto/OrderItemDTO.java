20c1af5b20fae81bfb2c27bcfbc9b1a5ac10a918
Caue Garcia <cauerg7@outlook.com>
2023-12-20 21:32:21 -0300

    saving order erro 415

---
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
@@ -0,0 +1,45 @@
+package com.devsuperior.dscommerce.dto;
+
+import com.devsuperior.dscommerce.entities.OrderItem;
+
+public class OrderItemDTO {
+
+	private Long productId;
+	private String name;
+	private Integer quantity;
+	private Double price;
+	
+	public OrderItemDTO(Long productId, String name, Integer quantity, Double price) {
+		this.productId = productId;
+		this.name = name;
+		this.quantity = quantity;
+		this.price = price;
+	}
+	
+	public OrderItemDTO(OrderItem entity) {
+		productId = entity.getProduct().getId();
+		name = entity.getProduct().getName();
+		quantity = entity.getQuantity();
+		price = entity.getPrice();
+	}
+
+	public Long getProductId() {
+		return productId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+	
+	public Double getSubTotal() {
+		return price * quantity;
+	}
+}
