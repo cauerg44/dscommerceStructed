622dccbfc0191596dbb1886d9ff52a47cde5473d
Caue Garcia <cauerg7@outlook.com>
2023-12-21 11:03:26 -0300

    fix validation

--- src/main/java/com/devsuperior/dscommerce/dto/ValidationErrorDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ValidationErrorDTO.java
@@ -8,7 +8,6 @@ public class ValidationErrorDTO extends CustomErrorDTO {
 
     private List<FieldMessageDTO> errors = new ArrayList<>();
 
-
     public ValidationErrorDTO(Instant timestamp, Integer status, String error, String path) {
         super(timestamp, status, error, path);
     }
@@ -18,6 +17,7 @@ public class ValidationErrorDTO extends CustomErrorDTO {
     }
 
     public void addError(String fieldName, String message) {
+    	errors.removeIf(x -> x.getFieldName().equals(fieldName));
         errors.add(new FieldMessageDTO(fieldName, message));
     }
 }
